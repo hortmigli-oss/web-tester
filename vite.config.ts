a17fa4824dcdcc1389015a9bd4bf918b3cc69ea5
hortmigli-oss <hortmigli@gmail.com>
2026-08-25 08:55:36 +0000

    для deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Для корректной работы на GitHub Pages
+  base: '/web-tester/', // Для корректной работы на GitHub Pages
 })
