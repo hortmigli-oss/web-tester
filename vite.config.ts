6ec0624ca5d22bcb53a29bb547b5584dab516c2d
hortmigli-oss <hortmigli@gmail.com>
2026-08-25 17:14:14 +0300

    Revert "Update from task e7d2917f-0d62-4505-8358-200f51d68b11"

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/web-tester/', // Для корректной работы на GitHub Pages
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
